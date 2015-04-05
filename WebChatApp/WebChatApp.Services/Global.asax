8d998939d2469a845b15eea59dbdfaa105952a21
SGStoyanov <stoyanov.vn@gmail.com>
2015-04-05 12:37:37 +0300

    Empty WebApi project added

---
+++ WebChatApp/WebChatApp.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebChatApp.Services.WebApiApplication" Language="C#" %>
---
+++ WebChatApp/WebChatApp.Services/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿namespace WebChatApp.Services
+{
+    using System.Web;
+    using System.Web.Http;
+
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
