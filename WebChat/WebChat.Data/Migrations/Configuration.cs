7998a41de8ef5a9851497760133100066f22163d
Zhivko Georgiev <zdgeorgiev@abv.bg>
2015-04-05 13:30:48 +0300

    Added WebAPI Sample Project with User Authentication + WebChatUser Model & WebChatDbContext

---
+++ WebChat/WebChat.Data/Migrations/Configuration.cs
@@ -0,0 +1,13 @@
+namespace WebChat.Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebChatDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = false;
+        }
+    }
+}
---
+++ WebChat/WebChat.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebChat.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
